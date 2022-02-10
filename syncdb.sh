fd2ec977d2179a2873693e96dc982d8b79c43b56
Maxim Batourine <35441642+batourin@users.noreply.github.com>
2022-02-10 11:28:31 -0500

    Use command line arg for pantheon env

--- syncdb.sh
+++ syncdb.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-terminus drush daniels-myaccount.dev -- sql:dump | docker exec -i localdev_db sh -c 'exec mysql -udrupal -pdrupal drupal'
+PANTHEON_ENV=$1
+
+terminus drush $PANTHEON_ENV -- sql:dump | docker exec -i localdev_db sh -c 'exec mysql -udrupal -pdrupal drupal'
